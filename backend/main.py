353bdfa96b246fb582a89b927dc67c01c5729561
Rhino <120325522+IamRhinooo@users.noreply.github.com>
2026-03-09 12:33:23 +0000

    Adds vite for frontend

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,8 @@ app = FastAPI(
 
 origins = [
     "http://localhost:3000",  # React frontend
+    "http://localhost:5173",  # Vite frontend
+    "http://127.0.0.1:5173",   
 ]
 app.add_middleware(
     CORSMiddleware,
